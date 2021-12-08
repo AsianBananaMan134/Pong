714a1662cd055df7511bce56074e27861cff127f
Allen U <91634751+AsianBananaMan134@users.noreply.github.com>
2021-12-08 13:02:12 -0800

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,5 +1,4 @@
 import turtle as trtl
-import threading
 
 # Turtle screen start
 wn = trtl.Screen()
@@ -36,7 +35,7 @@ Players 1 and 2 stuff
 p1 = trtl.Turtle()
 p1.speed(0)
 p1.shape("square")
-p1.color("white")
+p1.color("red")
 p1.shapesize(stretch_wid=5, stretch_len=1)
 p1.penup()
 p1.goto(-280, 0)
@@ -45,7 +44,7 @@ p1.goto(-280, 0)
 p2 = trtl.Turtle()
 p2.speed(0)
 p2.shape("square")
-p2.color("white")
+p2.color("blue")
 p2.shapesize(stretch_wid=5, stretch_len=1)
 p2.penup()
 p2.goto(280, 0)
@@ -56,32 +55,33 @@ Ball
 # Ball
 ball = trtl.Turtle()
 ball.speed(0)
-ball.shape("square")
+ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
 
 """
 Moving Functions
 """
+player_speed = 20
+
 # Player 1
 def move_up_1():
     y = p1.ycor()
     y += 20
     p1.sety(y)
-    if y < -280:
-        y = -280
+    # Makes it so it wont go out of bounds
+    if y > 250:
+        y = 250
     p1.sety(y)
-
 
 def move_down_1():
     y = p1.ycor()
     y -= 20
     p1.sety(y)
-    if y > 280:
-        y = 280
+    # Makes it so it wont go out of bounds
+    if y < -250:
+        y = -250
     p1.sety(y)
 
 # Player 2
@@ -89,44 +89,68 @@ def move_up_2():
     y = p2.ycor()
     y += 20
     p2.sety(y)
-    if y < -280:
-        y = -280
+    # Makes it so it wont go out of bounds
+    if y > 250:
+        y = 250
     p2.sety(y)
 
 def move_down_2():
     y = p2.ycor()
     y -= 20
     p2.sety(y)
-    if y > 280:
-        y = 280
+    # Makes it so it wont go out of bounds
+    if y < -250:
+        y = -250
     p2.sety(y)
-
-# Make it so they can both 
-if __name__ == "__main__":
-    p1u = threading.Thread(name="mu1", target=move_up_1())
-    p1u.start()
-    p1d = threading.Thread(name="md1", target=move_down_1())
-    p1d.start()
-    p2u = threading.Thread(name="mu2", target=move_up_2())
-    p2u.start()
-    p2d = threading.Thread(name="md2", target=move_down_2())
-    p2d.start()
-    p1u.join()
-    p1d.join()
-    p2u.join()
-    p2d.join()
-    exit(0)
-
-
-
 
 # Keyboard
 wn.listen()
-wn.onkeypress(move_up_1, "w")
-wn.onkeypress(move_down_1, "s")
-wn.onkeypress(move_up_2, "Up")
-wn.onkeypress(move_down_2, "Down")
+wn.onkey(move_up_1, "w")
+wn.onkey(move_down_1, "s")
+wn.onkey(move_up_2, "Up")
+wn.onkey(move_down_2, "Down")
 
+"""
+Score Variable
+"""
+# Score
+p1_score = 0
+p2_score = 0
+
+# Score Writer for player 1
+score_writer1 = trtl.Turtle()
+score_writer1.speed(0)
+score_writer1.color("white")
+score_writer1.penup()
+score_writer1.hideturtle()
+score_writer1.goto(-290, 260)
+score_writer1.pendown()
+font_setup = ("Arial", "15", "normal")
+score_writer1.write(str(p1_score) + " points", font = font_setup)
+
+# Score Writer for player 1
+score_writer2 = trtl.Turtle()
+score_writer2.speed(0)
+score_writer2.color("white")
+score_writer2.penup()
+score_writer2.hideturtle()
+score_writer2.goto(220, 260)
+score_writer2.pendown()
+font_setup = ("Arial", "15", "normal")
+score_writer2.write(str(p2_score) + " points", font = font_setup)
+
+# Update score function
+def update_score1():
+  score_writer1.clear()
+  global p1_score
+  p1_score += 10
+  score_writer1.write(str(p1_score) + " points", font = font_setup)
+
+def update_score2():
+  score_writer2.clear()
+  global p2_score
+  p2_score += 10
+  score_writer2.write(str(p2_score) + " points", font = font_setup)
 
 """
 While True
